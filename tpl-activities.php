277f3144b8ca5591c3d03d573ad8e382fe60c5a6
Kateriine <catherine@spade.be>
2016-03-05 12:47:37 +0100

    test commit

--- tpl-activities.php
+++ tpl-activities.php
@@ -4,6 +4,7 @@
  */
 ?>
 
+
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
